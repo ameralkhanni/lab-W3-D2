2a17a7ec9d742da036ce96ea94aec4565c8f0066
Amer <ameralkhanni@gmail.com>
2023-01-30 15:27:56 +0300

    My cv

---
+++ index.js
@@ -0,0 +1,122 @@
+// let es=document.getElementById("root")
+// let ds=document.getElementById("div")
+// ds.innerHTML("drfesse")
+// es.appendChild(ds)
+
+// const container=document.getElementById("root")
+// const elme=React.createElement("div",{
+// children:"Hi React"
+
+// });
+// ReactDOM.render(elme,container);
+const container=document.getElementById("root");
+let element = 
+<>
+<h1 class="name">Amer Mohammed Mohsen</h1>
+<p class="cont"><i class="fa-solid fa-envelope"></i> ameralkahnni@gmail.com <i class="fa-sharp fa-solid fa-phone"></i> +966532206924 <i class="fa-brands fa-linkedin"></i> amer-alKhanni <i class="fa-solid fa-location-dot"></i> Saudi Arabia, Riyadh</p> 
+
+
+{/* <img src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=906&q=80" alt="my "></img> */}
+
+
+<hr class="hrr"></hr>
+
+<h2><i class="fa-solid fa-address-card"></i> PROFIL</h2>
+<div>
+<p class="tee">A fresh graduate in computer
+science interested in all areas,
+especially in the development of
+Front-end, I have experience using
+JavaScript, HTML,and CSS.
+familiar with PHP , java, I have the
+enthusiasm and passion to learn
+new things and take on new
+challenges.</p>
+<hr></hr>
+
+
+<h2><i class="fa-solid fa-award"></i> EDUCATION</h2>
+</div>
+<p class ="tee"><b>2017-2022</b></p>
+
+    <p class="tee">
+        King Khalid University
+</p>
+
+<p class="tee">Bachelor of Computer Science,
+College of Computer Science, King
+Khalid University.</p>
+<p class ="tee"><b>2023/01-2023/03</b></p>
+<p class="tee">
+
+Tuwiq Academy</p>
+<p class="tee">
+JavaScript</p>
+
+<hr></hr>
+
+
+<h2><i class="fa-solid fa-toolbox"></i> SKILLS</h2>
+<div>
+    <lu>
+        
+        <li>Project Management Skills
+</li>
+        <li>Project Management Skills
+</li>
+        <li>Time management
+</li>
+        <li>Time management
+</li>
+
+
+    </lu>
+    <hr></hr>
+
+    <h2><i class="fa-solid fa-microchip"></i> TECHNOLOGIES SKILLS</h2>
+<div>
+    <lu>
+        
+        <li>Programming Language hardware
+HTML, CSS, Javascript, PHP, Java,c++
+</li>
+        <li>Other Technologies
+
+</li>
+        <li>React.js , Bootstrap, Git&GitHub
+
+</li>
+<li>hardware</li>
+        <li>Disassembly, installation and maintenance
+
+</li>
+</lu>
+</div>
+<hr></hr>
+
+<h2><i class="fa-solid fa-diagram-project"></i> PROJECTS</h2>
+<div>
+    <p class="tee"><b>Graduation Project</b>
+    
+</p>
+<p class="tee">
+Front-end, Back-end, Database, Server
+Used technologies: React.js, Node js,Express
+js, MongoDB, Bootstrap Its web app for
+management and measurement of Course
+Learning Outcomes
+</p>
+</div>
+<br></br>
+<br></br>
+<br></br>
+
+
+
+
+</div>
+
+</>
+ReactDOM.render(element,container);
+
+
